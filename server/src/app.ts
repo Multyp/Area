c960a32ee3a35a40c6792eeb213defc0a1d0f45e
Multyp <miguel.dasilva2004@hotmail.com>
2024-11-10 07:15:04 +0000

    edit: fixed swagger endpoint

--- server/src/app.ts
+++ server/src/app.ts
@@ -32,7 +32,7 @@ const swaggerOptions = {
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json());
 app.use(cookieParser());
